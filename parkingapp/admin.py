a9f2c3fef74ebce382b595c2ba459f98ee2deeec
Henry Giwa <favourgiwa3@gmail.com>
2023-04-14 12:52:10 +0100

    implemented the stripe payment gateway

--- parkingapp/admin.py
+++ parkingapp/admin.py
@@ -1,45 +1,33 @@
 from django.contrib import admin
-# Register your models here.
 from .models import CustomerProfile, CustomerVehicle, CreditCard, ParkingLot, ParkingReservation, ParkingSpaceBooking
 from django.utils.safestring import mark_safe
+from django.urls import reverse
+from django.utils.html import format_html
+
 
 # Register your models here.
 admin.site.register(CustomerProfile)
 admin.site.register(CreditCard)
 admin.site.register(ParkingLot)
-admin.site.register(ParkingReservation)
 admin.site.register(CustomerVehicle)
 
 
-
-#from parkingapp.models import Payment
-
 class ParkingReservationAdmin(admin.ModelAdmin):
-    # existing code for the admin class
+    list_display = ('users_id', 'Customervehicle_id', 'parking_lot', 'start_time', 'end_time', 'is_active', 'is_paid', 'custom_button')
     
-    def pay(self, request, queryset):
-        for reservation in queryset:
-            payment = Payment.objects.create(
-                amount=reservation.cost,
-                card_number=reservation.card_number,
-                cardholder_name=reservation.cardholder_name,
-                expiration_month=reservation.expiration_month,
-                expiration_year=reservation.expiration_year,
-                cvv=reservation.cvv
-            )
-            payment.charge()
-            reservation.is_paid = True
-            reservation.save()
+    def custom_button(self, obj):
+        url = reverse('parkingapp:create_checkout_session')
+        return format_html('<a class="button" href="{}">Pay</a>', url)
     
-    pay.short_description = "Mark selected reservations as paid"
-    actions = [pay]
+    custom_button.short_description = 'Pay'
 
-#admin.site.register(ParkingReservation, ParkingReservationAdmin)
+
+admin.site.register(ParkingReservation, ParkingReservationAdmin)
 
 
 @admin.register(ParkingSpaceBooking)
 class ParkingSpaceBookingAdmin(admin.ModelAdmin):
-    list_display = ('user', 'parking_space', 'is_paid')
+    list_display = ('user', 'parking_reservation', 'is_paid', 'parking_lot')
     list_filter = ('is_paid',)
     search_fields = ('user__email', 'parking_space__name', 'Customervehicle__license_plate')
     actions = ('mark_as_paid',)
@@ -47,7 +35,3 @@ class ParkingSpaceBookingAdmin(admin.ModelAdmin):
     def mark_as_paid(self, request, queryset):
         queryset.update(is_paid=True)
     # Your admin options here
-
-    
-
-
--- parkingapp/urls.py
+++ parkingapp/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name='parkingapp'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.homepage, name='homepage'),
     path('loginadmin/', views.loginadmin, name='loginadmin'),
-    path('userLogin/', views.userLogin, name='userLogin'),
-    path('userSignup/', views.userSignup, name='userSignup'),
     path('DashboardPage/',views.DashboardPage,name='DashboardPage'),
-    path('exit_vehicle/', views.exit_vehicle, name='exit_vehicle'),
-    path('payment_success/', views.payment_success, name='payment_success'), # add this line
-    path('reservation/<int:reservation_id>/payment/', views.payment_view, name='payment'),
+    path('product_page/', views.product_page, name='product_page'),
+    path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
+	path('payment_successful/', views.payment_successful, name='payment_successful'),
+	path('payment_cancelled/', views.payment_cancelled, name='payment_cancelled'),
+	path('stripe_webhook/', views.stripe_webhook, name='stripe_webhook'),
 ]
